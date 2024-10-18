73c494b6070bc9610edf01e49e80127e3b45d8a3
norf-90 <blacknorfolk@gmail.com>
2024-10-18 14:52:37 +0300

    config image minimaze plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from "vite";
 import ViteSvgSpriteWrapper from "vite-svg-sprite-wrapper";
 
+import viteImagemin from "@vheemstra/vite-plugin-imagemin";
+import imageminMozjpeg from "imagemin-mozjpeg";
+import imageminWebp from "imagemin-webp";
+
 export default defineConfig({
   plugins: [
     ViteSvgSpriteWrapper({
       icons: "./src/assets/images/svg/*.svg",
       outputDir: "./public/images/svg",
+    }),
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+        },
+      },
     }),
   ],
 });
